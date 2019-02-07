0354608f44acea7208e543fcb0970aef146b5f4d
Henrik Irgens Gravdal <hgravdal@bindeleddet.ntnu.no>
2019-02-07 22:55:24 +0100

    Add user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chatroom#index'
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
 end
